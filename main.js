9647d7992bf87db7e1ca421c99c594b1d16cb5f9
Shereen <90640451+shereenetemad@users.noreply.github.com>
2025-03-26 08:07:39 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>LCM VISUAL TEST</title>
+  <style>
+    body { margin: 0; }
+    canvas { 
+      display: block;
+      width: 100vw;
+      height: 100vh;
+    }
+  </style>
+</head>
+<body>
+  <canvas id="canvas"></canvas>
+  <script src="main.js"></script> <!-- REMOVED 'defer' -->
+</body>
+</html>
